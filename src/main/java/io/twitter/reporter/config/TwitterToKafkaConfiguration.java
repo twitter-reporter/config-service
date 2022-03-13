c85be8b3d7e04974419f0b2bcc23a9b15c8e7806
Mohsen Ebrahimi <mohsen.ebrahimi@auto1.com>
2022-03-13 20:19:39 +0100

    Initialize config-service

---
+++ src/main/java/io/twitter/reporter/config/TwitterToKafkaConfiguration.java
@@ -0,0 +1,16 @@
+package io.twitter.reporter.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Data
+@Configuration
+@ConfigurationProperties("twitter.to.kafka")
+public class TwitterToKafkaConfiguration {
+
+    private List<String> twitterKeywords;
+
+}
